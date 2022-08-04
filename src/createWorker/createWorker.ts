ff5371a3b5649059cca959fa3c350443de53e6b1
parsasi <mail@parsa.pro>
2022-08-04 10:39:01 -0700

    Added a check for `Worker` support

--- src/createWorker/createWorker.ts
+++ src/createWorker/createWorker.ts
@@ -3,5 +3,8 @@ import type { DutyWorkerOptions, DutyCB } from "../types";
 
 export const createWorker = (code: DutyCB, options?: DutyWorkerOptions) => {
   const urlObject = convertFnToObjectUrl(code);
+  if (typeof Worker === undefined) {
+    throw new Error("Your browser does not support web workers");
+  }
   return new Worker(urlObject, options);
 };
